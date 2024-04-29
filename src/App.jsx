cfece05a5d9f460ddc30cd462bc36b86c37d243c
Sanowar72 <atifreza72ar@gmail.com>
2024-04-29 21:52:04 +0530

    inline css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const increaseOne = () => {
-    setCount(count + 1);
-  };
   return (
-    <>
-      <h1>this is count value {count}</h1>
-      <button onClick={increaseOne}>add one</button>
-      <button onClick={() => setCount(0)}>reset to 0</button>
-      <button
-        onClick={() => {
-          setCount((pre) => {
-            if (pre > 0) {
-              return pre - 1;
-            } else {
-              return 0;
-            }
-          });
-        }}
-      >
-        substract one
-      </button>
-    </>
+    <div
+      style={{
+        color: "red",
+        marginBottom: "10px",
+        marginTop: "100px",
+        fontSize: "24px",
+      }}
+    >
+      App
+    </div>
   );
 };
 
